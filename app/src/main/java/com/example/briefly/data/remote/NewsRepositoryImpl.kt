04ef5bcfb5cc8f446830e48ff9cf857bfd518ce1
amy6 <mahima.6.ms@gmail.com>
2025-06-28 13:39:37 +0530

    Add Repository layer changes

---
+++ app/src/main/java/com/example/briefly/data/remote/NewsRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.example.briefly.data.remote
+
+import com.example.briefly.core.Result
+import com.example.briefly.data.remote.dto.toNewsResponse
+import com.example.briefly.domain.model.NewsItem
+import com.example.briefly.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
+
+class NewsRepositoryImpl(
+    private val newsApiService: NewsApiService
+) : NewsRepository {
+    override suspend fun getTopHeadlines(
+        country: String,
+        apiKey: String
+    ): Flow<Result<List<NewsItem>>> = flow {
+        try {
+            emit(Result.Loading())
+            val response = newsApiService.getTopHeadlines(country, apiKey).toNewsResponse()
+            emit(Result.Success(response.articles))
+        } catch (e: HttpException) {
+            emit(Result.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Result.Error("Couldn't reach server. Check your internet connection."))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/briefly/domain/repository/NewsRepository.kt
@@ -0,0 +1,9 @@
+package com.example.briefly.domain.repository
+
+import com.example.briefly.core.Result
+import com.example.briefly.domain.model.NewsItem
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+    suspend fun getTopHeadlines(country: String, apiKey: String): Flow<Result<List<NewsItem>>>
+}
